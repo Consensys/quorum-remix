a70d9d28ab4fce4cdab622f0e9e01c53680a2b00
bmcd87 <bradmcdermott@gmail.com>
2019-10-08 12:55:26 -0400

    Adds read-only tesseraEndpoint div

--- src/components/Network.js
+++ src/components/Network.js
@@ -5,11 +5,13 @@ export function Network () {
   const {
     provider,
     endpoint,
+    tesseraEndpoint,
     details: { name, id }
   } = React.useContext(Store).state.network
 
   return <div>
     <div><strong>Network: </strong>{name} ({id})</div>
     <div>{endpoint} ({provider})</div>
+    <div>Tessera endpoint: {tesseraEndpoint}</div>
     </div>
 }
