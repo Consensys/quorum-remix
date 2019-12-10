7ba551dddd7fee6cd47ec0435cf7d5409de9131a
bmcd87 <bradmcdermott@gmail.com>
2019-12-10 13:39:27 -0500

    Removes /qr/ from the path

---
+++ precache-manifest.b6a63c9a2186b055776f32e6759d6ff6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b08c50af99082e907bf605ea81d1dff1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a9d3a3f07e19688d5aef",
+    "url": "/static/css/main.2680dfa7.chunk.css"
+  },
+  {
+    "revision": "8df875b9f794e2a03246",
+    "url": "/static/js/2.84746441.chunk.js"
+  },
+  {
+    "revision": "a9d3a3f07e19688d5aef",
+    "url": "/static/js/main.84ce646c.chunk.js"
+  },
+  {
+    "revision": "2caf420b2cc25ecf79c6",
+    "url": "/static/js/runtime-main.bc4d8410.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/qr/precache-manifest.02f772d841d1215e777af734e7c6f586.js"
+  "/precache-manifest.b6a63c9a2186b055776f32e6759d6ff6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/qr/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
