da694a5f69412fd4db423369c1eb234d7fb8f8da
Brad McDermott <bradmcdermott@gmail.com>
2020-11-20 14:47:47 -0500

    Removes devMode for now, was causing issues and doesn't seem to be needed

--- src/utils/EnvUtils.js
+++ src/utils/EnvUtils.js
@@ -2,28 +2,6 @@ export function isDevelopment () {
   return process.env.NODE_ENV === 'development'
 }
 
-export function getPluginDevMode () {
-  if (isDevelopment()) {
-    return {
-      port: 8080,
-      origins: [
-        'http://localhost',
-        'http://remix-dev.goquorum.com',
-        'https://remix-dev.goquorum.com',
-      ],
-    }
-  } else {
-    return {
-      origins: [
-        'http://remix-dev.goquorum.com',
-        'https://remix-dev.goquorum.com',
-        'http://remix-staging.goquorum.com',
-        'https://remix-staging.goquorum.com',
-      ],
-    }
-  }
-}
-
 export function saveToLocalStorage(key, value) {
   try {
     localStorage[key] = value
