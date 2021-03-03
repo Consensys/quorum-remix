f774b60f76d795e8333159d275d674907e247719
bmcd <bmcd@users.noreply.github.com>
2021-03-03 19:37:56 +0000

    Build of #c179fc36

---
+++ precache-manifest.b4125c21dd8d56aec049b05e948a24df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc841c0336d655006cad98d499462c72",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a8cad5f001342a2a78b9",
+    "url": "/static/css/main.32fa3b44.chunk.css"
+  },
+  {
+    "revision": "f9df2dbf4a7dab690828",
+    "url": "/static/js/2.14f32786.chunk.js"
+  },
+  {
+    "revision": "05d2a0fb50359fe53f7daf9b5103332d",
+    "url": "/static/js/2.14f32786.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8cad5f001342a2a78b9",
+    "url": "/static/js/main.509604ce.chunk.js"
+  },
+  {
+    "revision": "7e1fca976aa57afebb11",
+    "url": "/static/js/runtime-main.2fd9dd60.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5095f860c82f0d22d8a4da7c831d273d.js"
+  "/precache-manifest.b4125c21dd8d56aec049b05e948a24df.js"
 );
 
 self.addEventListener('message', (event) => {
